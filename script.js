d68edd48e0f87fbaf2d53534d08b1847daeb9158
Mihai Apopi <151558116+mihai1702@users.noreply.github.com>
2024-11-26 10:23:09 +0200

    final

--- script.js
+++ script.js
@@ -14,28 +14,34 @@ window.onclick = function (event) {
         }
     }
 };
-function swipePhotos(){
-var swiper = new Swiper(".swiper", {
-    effect:"coverflow", 
-    grabCursor:true,
-    centeredSlides:true,
-    slidesPerView:2,
-    speed:600,
-    coverflowEffect:{
-        rotate:50,
-        stretch:0,
-        depth:100,
-        modifier:1,
-        slideShadows:true,
+function swipePhotos() {
+  var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 2, // Valoarea implicită
+    speed: 600,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
     },
     loop: true,
     autoplay: {
-      delay: 2000, // Timp între tranziții automate (3 secunde)
+      delay: 2000, // Timp între tranziții automate
       disableOnInteraction: false, // Continuă autoplay chiar dacă utilizatorul interacționează
     },
-
-});
+    breakpoints: {
+      // Setări pentru dimensiuni mai mari
+      1024: { // Ecran cu lățime de minim 1024px
+        slidesPerView: 3,
+      },
+    },
+  });
 }
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const slider = document.querySelector(".swiper");
@@ -50,3 +56,26 @@ document.addEventListener("DOMContentLoaded", () => {
   
     observer.observe(slider);
   });
+
+  window.addEventListener('scroll', function(){
+    var scrollPosition = window.scrollY;
+    var element = document.getElementById('page-up');
+
+    if(scrollPosition>500){
+      element.style.display='block';
+    }
+    else{
+      element.style.display='none';
+    }
+
+  })
+
+
+
+// function ScrollToTop(event) {
+//     event.preventDefault(); // Previne comportamentul implicit al link-ului
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }
